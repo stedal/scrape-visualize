80b8f97fc15bf56f26d05450a3d9cc12f4e664aa
Allen <allenstedman@gmail.com>
2018-12-07 17:43:14 -0800

    switched data

--- herokuapp/main.py
+++ herokuapp/main.py
@@ -8,19 +8,19 @@ from bokeh.io import output_notebook, show
 from bokeh.layouts import row,column, widgetbox
 from bokeh.io import curdoc,show, output_file
 
-df = pd.read_csv('herokuapp/data/lx570.csv')
+df = pd.read_csv('herokuapp/data/corolla.csv')
 
 source = ColumnDataSource(df)
 #x = df.productionDate
 #y = df.price
 #output_file("openurl.html")
-p1 = figure(title = "LX570 cost per year", plot_width = 900, plot_height = 900,
+p1 = figure(title = "Corolla cost per year", plot_width = 900, plot_height = 900,
            tools="pan,wheel_zoom,box_zoom,reset,tap")
 p1.circle(x = 'productionDate' ,y = 'price', size = 5, source = source)
 p1.xaxis.axis_label = 'Year'
 p1.yaxis.axis_label = 'Price'
 p1.left[0].formatter.use_scientific = False
-hover = HoverTool(tooltips = [("Year", "@productionDate"), ('Price', '@price'), ('Mileage', '@mileage')])
+hover = HoverTool(tooltips = [("Year", "@productionDate"), ('Price', '@price'), ('Mileage', '@mileage'), ('Trim','@trim')])
 p1.add_tools(hover)
 url = "https://@url"
 taptool = p1.select(type = TapTool)
